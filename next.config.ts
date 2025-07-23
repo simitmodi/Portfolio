a94497b09045cde56fa85a38e906b18d47c4862e
Simit Modi <simitjmodi@gmail.com>
2025-07-23 18:06:40 +0000

    now make it ready to push to git with the index.html file and all the ne

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,16 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
+g  },
   images: {
+    unoptimized: true,
     remotePatterns: [],
   },
 };
