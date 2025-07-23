05ec54d64684154ad02cb9790df69c8f0af39d1c
Simit Modi <simitjmodi@gmail.com>
2025-07-23 18:07:22 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.ts
@@ -0,0 +1,19 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [],
+  },
+};
+
+export default nextConfig;
